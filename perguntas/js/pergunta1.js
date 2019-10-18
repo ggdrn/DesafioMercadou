87c6659bce46c1e02c67c8c848354bf88cb05429
Gustavo Garcia <gustavo.ggdrn@gmail.com>
2019-10-17 21:31:35 -0300

    Verificação dos numeros primos, usando Vue com validação

--- perguntas/js/pergunta1.js
+++ perguntas/js/pergunta1.js
@@ -1,7 +1,42 @@
 new Vue({
   el:"#app",
   data: {
-    link: "https://github.com/ggdrn/DesafioMercadou"
-  }
-
+    value: null,
+    error: "",
+    result: []
+  },
+  methods: {
+    add(){
+      alert(this.value);
+    },
+    primo(num){
+     // Primeiro, vamos verificar se o numero digitado é "1", pois este numero
+     // não é primo primo
+     if(num!=1){
+      for (let i = 2; i < num; i++)
+        if (num % i == 0) return false;
+      return num !== 1;
+      }
+  },
+  verificaPrimo(){
+    // Vamos iniciar a variável que vai receber o resultado como um objeto vazio, para zerar o resultado.
+    this.result = [];
+    // verifica se é número
+    if(this.value == null){
+      this.error = "Digite algum valor";
+    }
+    else if(!isNaN(this.value)){
+      // verifica se é primo
+      for(let i=1;i<= this.value ;i++){
+        if (this.primo(i)) {
+          this.result.push(i);
+          this.error = "";
+        }
+      }
+    }
+    else this.error = "Insira um valor válido";
+  // Ao final do processo, vamos adicionar null ao valor do campo input
+  this.value = null;
+}
+}
 })
