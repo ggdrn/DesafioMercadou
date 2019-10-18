9ccc656e3b10671945b16c070a571618774cbf0b
Gustavo Garcia <gustavo.ggdrn@gmail.com>
2019-10-17 22:57:04 -0300

    Comentarios ao arquivo pergunta1.js adcionado

--- perguntas/js/pergunta1.js
+++ perguntas/js/pergunta1.js
@@ -7,22 +7,23 @@ new Vue({
   },
   methods: {
     // O código feito aqui é referente Questão 1, letra A
-    primo(num){
+    primo(value){
      // Primeiro, vamos verificar se o numero digitado é "1", pois este numero
-     // não é primo primo
-     if(num!=1){
-      for (let i = 2; i < num; i++)
-        if (num % i == 0) return false;
-      return num !== 1;
+     // não é primo
+     if(value!=1){
+      for (let i = 2; i < value; i++)
+        if (value % i == 0) return false;
+      return value !== 1;
       }
   },
   verificaPrimo(){
     // Vamos iniciar a variável que vai receber o resultado como um objeto vazio, para zerar o resultado.
     this.result = [];
-    // verifica se é número
+    // verifica se é nulo
     if(this.value == null){
       this.error = "Digite algum valor";
     }
+    // verifica se é um numero
     else if(!isNaN(this.value)){
       // verifica se é primo
       for(let i=1;i<= this.value ;i++){
@@ -33,7 +34,7 @@ new Vue({
       }
     }
     else this.error = "Insira um valor válido";
-  // Ao final do processo, vamos adicionar null ao valor do campo input
+  // Ao final do processo, vamos adicionar null ao valor do campo input, para limpar a entrada
   this.value = null;
 }
 }
