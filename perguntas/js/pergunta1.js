9d98789d60303d5017d211cbf27e0d3937195d14
Gustavo Garcia <gustavo.ggdrn@gmail.com>
2019-10-18 17:08:58 -0300

    Pergunta1.b resolvida

--- perguntas/js/pergunta1.js
+++ perguntas/js/pergunta1.js
@@ -25,37 +25,36 @@ new Vue({
   methods: {
     // O código feito aqui é referente Questão 1, letra A
     primo(value){
-     // Primeiro, vamos verificar se o numero digitado é "1", pois este numero
-     // não é primo
+     // Primeiro, vamos verificar se o numero digitado é "1", pois este numero não é primo
      if(value!=1){
       for (let i = 2; i < value; i++)
         if (value % i == 0) return false;
       return value !== 1;
       }
-  },
-  verificaPrimo(){
-    // Vamos iniciar a variável que vai receber o resultado como um objeto vazio, para zerar o resultado.
-    this.a.result = [];
-    // verifica se é nulo
-    if(this.a.value == null){
-      this.a.error = "Digite algum valor";
-    }
-    // verifica se é um numero
-    else if(!isNaN(this.a.value)){
-      // verifica se é primo
-      for(let i=1;i<= this.a.value ;i++){
-        if (this.primo(i)) {
-          this.a.result.push(i);
-          this.a.error = "";
+    },
+    verificaPrimo(){
+      // Vamos iniciar a variável que vai receber o resultado como um objeto vazio, para zerar o resultado.
+      this.a.result = [];
+      // verifica se é nulo
+      if(this.a.value == null){
+        this.a.error = "Digite algum valor";
+      }
+      // verifica se é um numero
+      else if(!isNaN(this.a.value)){
+        // verifica se é primo
+        for(let i=1;i<= this.a.value ;i++){
+          if (this.primo(i)) {
+            this.a.result.push(i);
+            this.a.error = "";
+          }
         }
       }
-    }
-    else this.a.error = "Insira um valor válido";
-    // Ao final do processo, vamos adicionar null ao valor do campo input, para limpar a entrada
-    this.a.value = null;
-    },
-    // O código feito até aqui é referente Questão 1, letra B
-    balaceamento(){
+      else this.a.error = "Insira um valor válido";
+      // Ao final do processo, vamos adicionar null ao valor do campo input, para limpar a entrada
+      this.a.value = null;
+      },
+      // O código feito até aqui é referente Questão 1, letra B
+    balancing(){
       if(this.b.value == null){
         this.b.error = "Digite algum valor";
         return; // Usamos um return para interromper a função
@@ -81,10 +80,6 @@ new Vue({
       }
       // A variável validate é responsável por informar se o dado irá está balanceado
       let validate = true;
-      // Vamos usar um array para pegar o indice de cada dado válido para o balanceamento.
-      // se não conter o dado esperado, o array valores negativos e comparar com seu par, que é referente
-      // ao valor esperado ex: dado entrado foi ()[, no array vai ficar 0 1 2 -1 -1 -1. Se seu par comparado for
-      // menor que 0, logo será -1, com isso, determinamos o balanceamento.
       let array = [];
       array[0] = this.b.aux.indexOf("(");
       array[1] = this.b.aux.indexOf(")");
@@ -92,26 +87,32 @@ new Vue({
       array[3] = this.b.aux.indexOf("]");
       array[4] = this.b.aux.indexOf("{");
       array[5] = this.b.aux.indexOf("}");
-      if (array[0] > array[1] && array[0] >= 0 && array[1] >= 0)
-      	validate = false;
-      else if ((array[0] >= 0 && array[1] < 0)||(array[1] >= 0 && array[0] < 0))
+      console.log(array);
+      /* sabemos que cada index correspnde ao um valor positivo, então, se entre cada
+      par possuir algum inteiros positivo, i indice foi balanceado, se não, está desbalanceado */
+      if((array[0] == -1 && array[1] >= 0) || (array[0] >= 0 && array[1] == -1))
         validate = false;
 
-      if (array[2] > array[3] && array[2] >= 0 && array[3] >= 0)
-      	validate = false;
-      else if ((array[2] >= 0 && array[3] < 0)||(array[3] >= 0 && array[2] < 0))
+      if((array[2] == -1 && array[3] >= 0) || (array[2] >= 0 && array[3] == -1))
         validate = false;
 
-      if (array[4] > array[5] && array[4] >= 0 && array[5] >= 0)
+      if((array[4] == -1 && array[5] >= 0) || (array[4] >= 0 && array[5] == -1))
         validate = false;
-      else if ((array[4] >= 0 && array[5] < 0)||(array[5] >= 0 && array[4] < 0))
-        validate = false;
+
+      // if((array[0] == -1 && array[1] != 1) || (array[0] != 0 && array[1] == -1))
+      //   validate = false;
+      //
+      // if((array[2] == -1 && array[3] != 3) || (array[2] != 2 && array[3] == -1))
+      //   validate = false;
+      //
+      // if((array[4] == -1 && array[5] != 5) || (array[4] != 4 && array[5] == -1))
+      //   validate = false;
 
       if(validate){
         this.b.result = "O dado inserido: "+this.b.value+" está balanceado";
         this.b.error = "";
       }else
-      	  this.b.error = "O dado inserido: "+this.b.value+" está desbalanceado";
+    	  this.b.error = "O dado inserido: "+this.b.value+" está desbalanceado";
       // No fim da operação, vamos zerar o input
       this.b.value = null;
     }
